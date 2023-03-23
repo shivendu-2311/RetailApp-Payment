0847c6031f4e85a654008b7071b2836ba81d5764
shivendu-2311 <81577820+shivendu-2311@users.noreply.github.com>
2023-03-23 23:56:17 +0530

    Delete PaymentService directory

--- PaymentService/src/main/java/com/retailApp/PaymentService/PaymentServiceDao/PaymentDao.java
+++
@@ -1,9 +0,0 @@
-package com.retailApp.PaymentService.PaymentServiceDao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.retailApp.PaymentService.POJO.Payment;
-
-public interface PaymentDao extends JpaRepository<Payment, Integer>{
-	
-}
